82a8d19d3b6078ea0df19184237d8b3eed91016f
Rustam Asbapov <r.asbapov@tbank.ru>
2025-03-03 11:02:20 +0500

    feat: configurate scrapper

---
+++ scrapper/src/main/java/backend/academy/scrapper/configuration/ScrapperConfig.java
@@ -0,0 +1,17 @@
+package backend.academy.scrapper.configuration;
+
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
+public record ScrapperConfig(
+    @NotEmpty String githubToken,
+    @NotEmpty String githubUri,
+    StackOverflowCredentials stackOverflow,
+    @NotEmpty String stackOverflowUri
+) {
+    public record StackOverflowCredentials(@NotEmpty String key, @NotEmpty String accessToken) {
+    }
+}
