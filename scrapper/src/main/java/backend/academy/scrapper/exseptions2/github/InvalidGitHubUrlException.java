77ab6454f55ab9a2bbbb398bf66818e8a5f94840
Rustam Asbapov <r.asbapov@tbank.ru>
2025-03-08 22:03:07 +0500

    refactor: delete unnecessary files

--- scrapper/src/main/java/backend/academy/scrapper/exseptions2/github/InvalidGitHubUrlException.java
+++
@@ -1,7 +0,0 @@
-package backend.academy.scrapper.exseptions2.github;
-
-public class InvalidGitHubUrlException extends RuntimeException {
-    public InvalidGitHubUrlException(String message) {
-        super(message);
-    }
-}
--- scrapper/src/main/java/backend/academy/scrapper/exseptions2/github/NoCommitsException.java
+++
@@ -1,7 +0,0 @@
-package backend.academy.scrapper.exseptions2.github;
-
-public class NoCommitsException extends RuntimeException {
-    public NoCommitsException() {
-        super("No commits found in repository");
-    }
-}
