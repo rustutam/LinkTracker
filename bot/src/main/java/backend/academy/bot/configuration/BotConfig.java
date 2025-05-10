21a7363e523eeafa23639b3610ad97894a6dbb98
rustutam <Rustam.Asbapov@urfu.me>
2025-05-10 23:37:25 +0500

    feat: add kafka configurations in bot

--- bot/src/main/java/backend/academy/bot/configuration/BotConfig.java
+++ bot/src/main/java/backend/academy/bot/configuration/BotConfig.java
@@ -1,9 +1,30 @@
 package backend.academy.bot.configuration;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record BotConfig(@NotEmpty String telegramToken, @NotEmpty String baseUrl) {}
+public record BotConfig(
+    @NotEmpty String telegramToken,
+    @NotEmpty String baseUrl,
+    @NotNull Kafka kafka
+) {
+    public record Kafka(@NotBlank String bootstrapServers,
+                        @NotNull @NotBlank String updateTopicName,
+                        @NotNull @NotBlank String dlqProcessingTopicName,
+                        @NotNull @NotBlank String dlqDeserializerTopicName,
+                        @NotNull @NotEmpty TopicsProperty[] topicsProperty) {
+    }
+
+    public record TopicsProperty(@NotBlank String topicName,
+                                 @NotNull @Positive Integer numberPartitions,
+                                 @NotNull @Positive Short replicationFactor) {
+
+    }
+
+}
---
+++ bot/src/main/java/backend/academy/bot/configuration/KafkaConfig.java
@@ -0,0 +1,13 @@
+package backend.academy.bot.configuration;
+
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "kafka", ignoreUnknownFields = false)
+public record KafkaConfig(LinkUpdatesTopicProperties linkUpdates) {
+    public record LinkUpdatesTopicProperties(
+            @NotEmpty String topic,
+            @NotEmpty boolean failOnProcessing,
+            @NotEmpty String consumerGroup,
+            @NotEmpty Integer concurrency) {}
+}
