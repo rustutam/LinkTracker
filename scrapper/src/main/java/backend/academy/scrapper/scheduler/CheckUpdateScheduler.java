9ede5a630a7d01a3d76b7a2e9edb58d79ce5a3ff
rustutam <Rustam.Asbapov@urfu.me>
2025-05-15 22:59:48 +0500

    feat: add scheduler for send updates

---
+++ scrapper/src/main/java/backend/academy/scrapper/scheduler/CheckUpdateScheduler.java
@@ -0,0 +1,24 @@
+package backend.academy.scrapper.scheduler;
+
+import backend.academy.scrapper.service.LinkProcessingService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import java.time.OffsetDateTime;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CheckUpdateScheduler {
+    private static final Integer LIMIT = 500;
+    private final LinkProcessingService linkProcessingService;
+
+    @Scheduled(fixedDelayString = "#{checkUpdateIntervalMs}")
+    public void checkUpdateJob() {
+        log.atInfo().setMessage("Запуск планировщика для проверки ссылок на обновления").log();
+        OffsetDateTime updateStartTime = OffsetDateTime.now();
+        linkProcessingService.processLinks(LIMIT, updateStartTime);
+        log.atInfo().setMessage("Завершение работы планировщика проверки ссылок").log();
+    }
+}
---
+++ scrapper/src/main/java/backend/academy/scrapper/scheduler/SendUpdateScheduler.java
@@ -0,0 +1,28 @@
+package backend.academy.scrapper.scheduler;
+
+import backend.academy.scrapper.models.domain.UpdatedLink;
+import backend.academy.scrapper.repository.database.LinkUpdateRepository;
+import backend.academy.scrapper.service.SenderNotificationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SendUpdateScheduler {
+    private final SenderNotificationService senderNotificationService;
+    private final LinkUpdateRepository linkUpdateRepository;
+
+    @Scheduled(fixedDelayString = "#{sendUpdateIntervalMs}")
+    public void sendUpdateJob() {
+        log.atInfo().setMessage("Запуск планировщика для отправки обновлений").log();
+        List<UpdatedLink> linksWithUpdate = linkUpdateRepository.findAll();
+        for (var updatedLink : linksWithUpdate) {
+            senderNotificationService.notifySender(updatedLink);
+        }
+        log.atInfo().setMessage("Завершение работы планировщика для отправки обновлений").log();
+    }
+}
