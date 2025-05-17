f76534378a32f14a7e210385d7ca68ea5abe2ad0
rustutam <Rustam.Asbapov@urfu.me>
2025-05-17 18:16:47 +0500

    feat: add 429 error

--- bot/src/main/java/backend/academy/bot/api/controllers/ControllerAdvice.java
+++ bot/src/main/java/backend/academy/bot/api/controllers/ControllerAdvice.java
@@ -1,16 +1,32 @@
 package backend.academy.bot.api.controllers;
 
 import backend.academy.bot.api.dto.ApiErrorResponse;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @RestControllerAdvice
 public class ControllerAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ApiErrorResponse.class)
     public ResponseEntity<ApiErrorResponse> handle(ApiErrorResponse error) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(RequestNotPermitted.class)
+    public ResponseEntity<ApiErrorResponse> handleRateLimitException(RequestNotPermitted ex) {
+        var response = new ApiErrorResponse(
+            "Too many requests.",
+            Integer.toString(HttpStatus.TOO_MANY_REQUESTS.value()),
+            ex.getClass().getName(),
+            "Too many requests.",
+            (ArrayList<String>) Arrays.stream(ex.getStackTrace())
+                .map(StackTraceElement::toString)
+                .toList());
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body(response);
     }
 }
--- bot/src/main/java/backend/academy/bot/api/controllers/UpdatesController.java
+++ bot/src/main/java/backend/academy/bot/api/controllers/UpdatesController.java
@@ -2,6 +2,7 @@ package backend.academy.bot.api.controllers;
 
 import backend.academy.bot.api.dto.LinkUpdate;
 import backend.academy.bot.api.services.UpdatesService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,7 @@ public class UpdatesController {
     }
 
     @PostMapping
+    @RateLimiter(name = "rateLimiter")
     public ResponseEntity<?> handleUpdates(@RequestBody LinkUpdate update) {
         updatesService.notifySubscribers(update);
         return new ResponseEntity<>(HttpStatus.OK);
